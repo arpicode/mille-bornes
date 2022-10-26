a8492a5bd64993a35a976da281f1b436f73d8bad
arpicode <arpicode@gmail.com>
2022-10-26 21:09:18 +0200

    Refactor

--- src/PileCartes.java
+++ src/PileCartes.java
@@ -1,28 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 public class PileCartes extends Stack<Carte> {
-    public PileCartes(Carte[] cartes) {
+    public PileCartes() {
+        super();
+    }
+
+    public PileCartes(ArrayList<Carte> cartes) {
         super();
         alimenter(cartes);
     }
 
-    public void alimenter(Carte[] cartes) {
-        for (int i = 0; i < cartes.length; i++) {
-            if (cartes[i].estValide()) {
-                this.push(cartes[i]);
+    public void alimenter(ArrayList<Carte> cartes) {
+        for (int i = 0; i < cartes.size(); i++) {
+            if (cartes.get(i).estValide()) {
+                this.push(cartes.get(i));
             }
         }
     }
 
-    // TODO à placer dans la classe fille Pioche
-    public static Carte[] melanger(Carte[] cartes) {
-        for (int lastIndex = cartes.length - 1; lastIndex > 0; lastIndex--) {
-            int pickedIndex = (int) Math.floor(Math.random() * (lastIndex + 1));
-            Carte tmp = cartes[lastIndex];
-            cartes[lastIndex] = cartes[pickedIndex];
-            cartes[pickedIndex] = tmp;
-        }
-        return cartes;
+    public static void melanger(List<Carte> cartes) {
+        Collections.shuffle(cartes);
     }
 
 }
