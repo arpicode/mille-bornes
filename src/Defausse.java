2af125930c2bb7828017e70b665b8ecb8ec9f6e8
arpicode <arpicode@gmail.com>
2022-10-30 20:03:15 +0100

    Add class

---
+++ src/Defausse.java
@@ -0,0 +1,11 @@
+/**
+ * Classe fille de PileCartes représentant la défausse.
+ * 
+ * @author Les Bornés
+ */
+public class Defausse extends PileCartes {
+
+    public Defausse() {
+        super();
+    }
+}
